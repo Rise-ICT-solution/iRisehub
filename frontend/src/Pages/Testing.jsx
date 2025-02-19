c1f835cd052f218a22de85c179a6ebed477a6646
Eng-Bakar <abuukarmuuri2018@gmail.com>
2025-02-19 11:22:38 +0300

    Pages ka Menu ku dhex jiro  aa waxaa ku daray sub pages iyo Waxaa ke dhex sameeyay folder ka pages files ka menu bar

--- frontend/src/Pages/Testing.jsx
+++
@@ -1,9 +0,0 @@
-// import Ecosystem from "../Components/Ecosystem"
-// // import Moving from "../Components/movingTest"
-
-// function TestPages (){ //This page is only for testing 
-//     return <div>
-//             <Ecosystem />
-//     </div>
-// }
-// export default TestPages //This page is only for testing 
