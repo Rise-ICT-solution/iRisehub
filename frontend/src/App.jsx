397708a257671b9f15f74980046bd80ef5ee6a9f
AB- nazER <updynasir001@gmail.com>
2025-02-22 10:59:46 +0300

    events wax laga qabtay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route,Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-import TestPages from "./Pages/Testing";
+// import TestPages from "./Pages/Testing";
+import Events from "./Pages/Community Pages/Events";
 function App (){ 
   return <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/test" element={<TestPages />} /> {/*This page line is only for testing */}
+    <Route path="/Events" element={<Events/>} />
+    {/* <Route path="/test" element={<TestPages />} /> This page line is only for testing */}
   </Routes>
 }
 export default App
